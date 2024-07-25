d26ba6b3be3f348efdd959326aa625758cca9340
Johnson noah <143536219+arishacke@users.noreply.github.com>
2024-07-25 09:40:47 -0700

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,77 +1,33 @@
-<?php
-session_start();
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-
-
-if(isset($_POST['submitContact']))
-{
-    $fullname = $_POST['full_name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-
-
-    //Create an instance; passing `true` enables exceptions
-    $mail = new PHPMailer(true);
-
-    try {
-        //Server settings
-        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        
-        $mail->Host       = 'trackfedgrant.us';                     //Set the SMTP server to send through
-        $mail->Username   = 'support@trackfedgrant.us';                     //SMTP username
-        $mail->Password   = 'Genesis123@';                               //SMTP password
-
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //ENCRYPTION_SMTPS 465 - Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-        //Recipients
-        $mail->setFrom('fundaofwebit@gmail.com', 'johnsonweb');
-        $mail->addAddress('ollarkunle1@gmail.com', 'johnsonWeb IT');     //Add a recipient
-
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'New enquiry - Funda of web it Contact Form';
-
-        $bodyContent = '<div>Hello, you got a new enquiry</div>
-            <div>Fullname: '.$fullname.'</div>
-            <div>Email: '.$email.'</div>
-            <div>Subject: '.$subject.'</div>
-            <div>Message: '.$message.'</div>
-        ';
-
-        $mail->Body = $bodyContent; 
-        
-        if($mail->send())
-        {
-            $_SESSION['status'] = "Thank you contact us - Team Funda of Web IT";
-            header("Location: {$_SERVER["HTTP_REFERER"]}");
-            exit(0);
-        }
-        else
-        {
-            $_SESSION['status'] = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-            header("Location: {$_SERVER["HTTP_REFERER"]}");
-            exit(0);
-        }
-        
-       
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    <?php
+    $errors = '';
+    $myemail = 'ollarkunle1@gmail.com';//<-----Put Your email address here.
+    if(empty($_POST['name'])  ||
+       empty($_POST['email']) ||
+       empty($_POST['message']))
+    {
+        $errors .= "\n Error: all fields are required";
     }
-}
-else
-{
-    header('Location: index.php');
-    exit(0);
-}
-
-?>
+    $name = $_POST['name'];
+    $email_address = $_POST['email'];
+    $message = $_POST['message'];
+    if (!preg_match(
+    "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+    $email_address))
+    {
+        $errors .= "\n Error: Invalid email address";
+    }
+    
+    if( empty($errors))
+    {
+    $to = $myemail;
+    $email_subject = "Contact form submission: $name";
+    $email_body = "You have received a new message. ".
+    " Here are the details:\n Name: $name \n ".
+    "Email: $email_address\n Message \n $message";
+    $headers = "From: $myemail\n";
+    $headers .= "Reply-To: $email_address";
+    mail($to,$email_subject,$email_body,$headers);
+    //redirect to the 'thank you' page
+    header('Location: index.html');
+    }
+    ?>
